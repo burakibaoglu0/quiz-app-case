8cf60b2e31a1c0979219bae0cfb0501aa807fbca
Burak İbaoğlu <buca_42@hotmail.com>
2023-07-14 02:31:52 +0300

    feat: Added timer element structure to the DOM

--- src/main.ts
+++ src/main.ts
@@ -68,27 +68,51 @@ try {
 
   renderQuestion();
 
+  const timerContainer = document.createElement('div');
+  timerContainer.classList.add('timer-container','non-selectable');
+
+  timerContainer.innerHTML = `
+    <svg height="200" width="200">
+      <circle cx="100" cy="100" r="75" stroke-width="0"/>
+      <text x="100" y="100" text-anchor="middle" dominant-baseline="middle" class="timer-text"></text>
+    </svg>
+  `;
+
   let _timer: NodeJS.Timer | null = null;
 
   const createTimer = () => {
     let _duration = 30;
 
     _timer = setInterval(() => {
+      timerContainer.classList.replace('selectable', 'non-selectable');
       if(_duration > 0){
         let _durationText =  `00:${_duration < 10 ? `0${_duration}` : _duration}s`;
+        (timerContainer.querySelector('svg text') as HTMLElement).innerHTML = _durationText;
+        timerContainer.querySelector('svg circle')?.setAttribute('stroke-dasharray', `${_duration * 3.6} ${_duration * 3.6}`);
+        timerContainer.querySelector('svg circle')?.setAttribute('stroke-dashoffset', `${_duration * 3.6}`);
+        timerContainer.querySelector('svg circle')?.setAttribute('stroke-width', `${_duration * 0.1}`);
+
         _duration--;
 
-        _duration <= 20 ? questionContainer.querySelector('.questions-body')?.classList.replace('non-selectable', 'selectable') : null;
+        if(_duration <= 20){
+          questionContainer.querySelector('.questions-body')?.classList.replace('non-selectable', 'selectable');
+          timerContainer.classList.replace('non-selectable', 'selectable');
+          timerContainer.querySelector('svg circle')?.setAttribute('stroke-width', '1');
+        }
       }else{
-        clearInterval(_timer!);
+        let _durationText =  `00:${_duration < 10 ? `0${_duration}` : _duration}s`;
+        (timerContainer.querySelector('svg text') as HTMLElement).innerHTML = _durationText;
+        timerContainer.classList.replace('selectable', 'non-selectable');
         handleClick();
       }
     }, 1000);
   }
+
   createTimer();
 
   const mainContainer = document.querySelector('#app');
   mainContainer?.appendChild(questionContainer);
+  mainContainer?.appendChild(timerContainer);
 } catch (e) {
   console.log(e);
 }
