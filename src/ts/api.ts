39eab65739d09e56a4d830f132a2842c7b560d51
Burak İbaoğlu <buca_42@hotmail.com>
2023-07-14 02:53:46 +0300

    refactor: Modify api.ts file

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -1,27 +1,28 @@
 import urlConfig from './config';
 import { IResponse, IQuestions } from './types';
 
-const answers = ['A','B','C','D'];
-const randomAnswer = () => {
-    return answers[Math.floor(Math.random() * answers.length)];
-}
+const answers = ['A', 'B', 'C', 'D'];
+const randomAnswer = (): string => {
+  return answers[Math.floor(Math.random() * answers.length)];
+};
 
-let questions : Array<IQuestions> | null = null;
+let questions: IQuestions[] | null = null;
 
-export const fetchQuestions = async () => {
-    if(questions) return questions;
+export const fetchQuestions = async (): Promise<IQuestions[] | null> => {
+  if (questions) return questions;
 
-    try{
-        const response = await fetch(urlConfig.BASE_URL);
-        const data : Array<IResponse> = await response.json();
-        questions = data.map((question: IResponse) => ({
-            title: question.title,
-            options: question.body.split('\n'),
-            answer: randomAnswer()
-        }));
+  try {
+    const response = await fetch(urlConfig.BASE_URL);
+    const data: IResponse[] = await response.json();
+    questions = data.map((question: IResponse) => ({
+      title: question.title,
+      options: question.body.split('\n'),
+      answer: randomAnswer()
+    }));
 
-        return questions?.slice(0,10);
-    }catch(err){
-        console.log(err);
-    }
-}
+    return questions?.slice(0, 10);
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
