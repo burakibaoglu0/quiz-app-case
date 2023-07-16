fb6c7d9a3cacb685acbeeef7ac2adf3b8703a5ba
Burak İbaoğlu <buca_42@hotmail.com>
2023-07-16 18:02:02 +0300

    feat: Updated selectionTable and styles

--- src/quiz-app/components/selectionTable/selectionTable.ts
+++ src/quiz-app/components/selectionTable/selectionTable.ts
@@ -2,32 +2,41 @@ import store from "../../store";
 import './selectionTable.scss';
 
 const renderUserSelectionsTable = () => {
-    const table = document.createElement('table');
+    const summaryContainer = document.createElement('div');
+    summaryContainer.classList.add('summary-container');
+    
     const userSelections = store.getters.getUserSelections(store.state);
 
-    table.classList.add('user-selections-table');
-    table.innerHTML = `
-    <thead>
-      <tr>
-        <th>Question</th>
-        <th>Answer</th>
-      </tr>
-    </thead>
-    <tbody>
-      ${userSelections.map((selection, index) => `
-        <tr>
-          <td>${index + 1}</td>
-          <td>${selection}</td>
-        </tr>
-      `).join('')}
-    </tbody>
-        `
+    summaryContainer.innerHTML = `
+        <div class="user-selections-table-container">
+          <div class="summary-title">
+            <h2>Your Selections</h2>
+          </div>
+          <table class="user-selections-table">
+            <thead>
+              <tr>
+                <th>Question</th>
+                <th>Answer</th>
+              </tr>
+            </thead>
+            <tbody>
+                ${userSelections.map((selection, index) => `
+                  <tr>
+                    <td>${index + 1}</td>
+                    <td>${selection}</td>
+                  </tr>
+                `).join('')}
+            </tbody>
+          </table>
+          <div class="summary-container-buttons">
+            <button class="summary-container-button" id="back-button"   onclick="window.location.reload()">Back</button">
+          </div>
+        </div>
+    `;
 
     document.querySelector('#app')?.querySelector('.question-container')?.classList.add('!hidden');
     document.querySelector('#app')?.querySelector('.timer-container')?.classList.add('!hidden');
-    document.querySelector('#app')?.appendChild(table);
-
-    return table;
+    document.querySelector('#app')?.appendChild(summaryContainer);
 }
 
 export default renderUserSelectionsTable;
